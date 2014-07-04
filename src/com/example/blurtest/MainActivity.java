64247b1ddbe0d9db8ce80f803bfd2e7954204912
jiangxiaoming <jiangxiaoming@conew.com>
2014-07-04 14:47:51 +0800

    增加對4.4全屏隱藏虛擬按鍵的支持

--- src/com/example/blurtest/MainActivity.java
+++ src/com/example/blurtest/MainActivity.java
@@ -19,11 +19,14 @@ public class MainActivity extends Activity implements
 	Bitmap mBitmap = null;
 	float mFac = 4;
 	int mKermalR = 20;
-
+	View mDecorView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		 mDecorView = getWindow().getDecorView();
 		setContentView(R.layout.activity_main);
+	       
 		mGestreDetector = new GestureDetector(this, this);
 		mLogMsg = (TextView) findViewById(R.id.log_msg);
 
@@ -56,22 +59,47 @@ public class MainActivity extends Activity implements
 						return true;
 					}
 				});
+		
+		hideSystemUI();
 
 	}
 
+	// This snippet hides the system bars.
+	private void hideSystemUI() {
+	    // Set the IMMERSIVE flag.
+	    // Set the content to appear under the system bars so that the content
+	    // doesn't resize when the system bars hide and show.
+	    mDecorView.setSystemUiVisibility(
+	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+	}
+
+	// This snippet shows the system bars. It does this by removing all the flags
+	// except for the ones that make the content appear under the system bars.
+	private void showSystemUI() {
+	    mDecorView.setSystemUiVisibility(
+	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+	}
+	
 	@Override
 	public boolean onDown(MotionEvent paramMotionEvent) {
+		hideSystemUI();
 		return true;
 	}
 
 	@Override
 	public void onShowPress(MotionEvent paramMotionEvent) {
-
+		showSystemUI();
 	}
 
 	@Override
 	public boolean onSingleTapUp(MotionEvent paramMotionEvent) {
-
 		return true;
 	}
 
